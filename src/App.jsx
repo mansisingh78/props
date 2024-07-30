c692765668d2f56fa6c1c1563968a18934b69c27
Mansi singh <mansi123singh1234@gmail.com>
2024-07-30 11:38:40 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@ import "./App.css";
 import Welcome from "./components/Welcome";
 
 function App() {
+  const biodata = {
+    name: "shradha",
+    age: 23,
+  };
+  const names = [
+    {
+      name: "shradha",
+      age: 23,
+    },
+    {
+      name: "hitesh",
+      age: 32,
+    },
+  ];
+
   let arr = [1, 2, 3, 4, 5];
   return (
     <>
-      <Welcome name="mansi" age={21} arr={arr} />
+      <Welcome name="mansi" age={21} arr={arr} info={biodata} names={names} />
     </>
   );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,4 @@
-const Welcome = ({ name, age, arr }) => {
+const Welcome = ({ name, age, arr, info, names }) => {
   return (
     <>
       <h1>
@@ -8,6 +8,14 @@ const Welcome = ({ name, age, arr }) => {
       {/* <h2>{arr}</h2> */}
       {arr.map((item, index) => (
         <h2 key={index}>{item}</h2>
+      ))}
+      <h2>{info.name}</h2>
+      <h2>{info.age}</h2>
+      {names.map((person, index) => (
+        <div key={index}>
+          <h2>{person.name}</h2>
+          <p>Age: {person.age}</p>
+        </div>
       ))}
     </>
   );
