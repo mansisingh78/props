44b9a3dd3db9a29185ccb518ec5947ca67a5240c
Mansi singh <mansi123singh1234@gmail.com>
2024-07-31 06:13:43 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   let arr = [1, 2, 3, 4, 5];
   return (
     <>
-      <Welcome name="mansi" age={21} arr={arr} info={biodata} names={names} />
+      <Welcome name="shradha" age={21} arr={arr} info={biodata} names={names} />
     </>
   );
 }
